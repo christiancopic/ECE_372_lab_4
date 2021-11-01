abba1bd500ccfe06798c14e9a1b5c2771f71ef5f
copic <silvercopic@gmail.com>
2021-11-01 14:50:48 -0700

    added pwm.h

--- lab_four/src/main.cpp
+++ lab_four/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+//git init test
+
 int main(){
 	while(1){
 
---
+++ lab_four/src/pwm.h
@@ -0,0 +1,8 @@
+#ifndef PWM_H
+#define PWM_H
+
+void changeDutyCycle();
+void initPWMTimer3();
+void initPWMTimer4();
+
+#endif
