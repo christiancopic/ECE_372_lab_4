b7a2e9219eb884fa5f064b388df8e8958f951486
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2021-11-01 15:10:12 -0700

    Add files via upload

---
+++ lab_four/src/pwm.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+void initPWMTimer3(){
+    //Set Pin number 5 to output
+    DDRE |= (1<<DDE3);
+
+    //WGM = 0111
+    TCCR3A |= (1<<WGM31)|(1<<WGM30);
+    TCCR3B |= (1<<WGM32);
+
+    //duty cycle to 25%
+    OCR3A = 255;
+    
+}
+
+
+void initPWMTimer4(){
+    //Set pin numner 6 to output
+    DDRH |= (1<<DDH3);
+    //WGM = 0111
+    TCCR4A |= (1<<WGM41)|(1<<WGM40);
+    TCCR4B |= (1<<WGM42);
+    //duty cycle to 25%
+    OCR4A = 255;
+}
