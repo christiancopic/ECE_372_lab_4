ac580c40733e59c480722b39fb6f60c9b69c3d2e
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2021-11-01 15:17:50 -0700

    Update pwm.cpp

--- lab_four/src/pwm.cpp
+++ lab_four/src/pwm.cpp
@@ -1,13 +1,14 @@
 
 
+#include <avr/io.h>
 
-
+//non-inverting
 void initPWMTimer3(){
     //Set Pin number 5 to output
     DDRE |= (1<<DDE3);
 
     //WGM = 0111
-    TCCR3A |= (1<<WGM31)|(1<<WGM30);
+    TCCR3A |= (1<<COM3A1)|(1<<WGM31)|(1<<WGM30);
     TCCR3B |= (1<<WGM32);
 
     //duty cycle to 25%
@@ -15,12 +16,12 @@ void initPWMTimer3(){
     
 }
 
-
+//inverting
 void initPWMTimer4(){
     //Set pin numner 6 to output
     DDRH |= (1<<DDH3);
     //WGM = 0111
-    TCCR4A |= (1<<WGM41)|(1<<WGM40);
+    TCCR4A |= (1<<COM4A1)|(1<<COM4A0)|(1<<WGM41)|(1<<WGM40);
     TCCR4B |= (1<<WGM42);
     //duty cycle to 25%
     OCR4A = 255;
