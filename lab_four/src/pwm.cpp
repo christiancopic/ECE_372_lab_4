a2eae3301c49efc3bda1ed19a0bdfb64ede15161
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2021-11-08 15:06:04 -0700

    Update pwm.cpp

--- lab_four/src/pwm.cpp
+++ lab_four/src/pwm.cpp
@@ -8,7 +8,7 @@ void initPWMTimer3(){
 
     //WGM = 0111
     TCCR3A |= (1<<COM3A1)|(1<<WGM31)|(1<<WGM30);
-    TCCR3B |= (1<<WGM32);
+    TCCR3B |= (1<<WGM32)|(1<<CS30);
 
     //duty cycle to 25%
     OCR3A = 255;
@@ -21,7 +21,7 @@ void initPWMTimer4(){
     DDRH |= (1<<DDH3);
     //WGM = 0111
     TCCR4A |= (1<<COM4A1)|(1<<COM4A0)|(1<<WGM41)|(1<<WGM40);
-    TCCR4B |= (1<<WGM42);
+    TCCR4B |= (1<<WGM42)|(1<<CS40);
     //duty cycle to 25%
     OCR4A = 255;
 }
